0a15b777c46ce84b84628fe19bec3fd9f5d4f5ba
Hans <hansvekoni@gmail.com>
2024-12-13 15:55:14 -0300

    refactor(deploy): update to ethers 6.0+ the configuration

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,25 +1,23 @@
 import { ethers } from "hardhat";
+
 async function main() {
-  const [deployer] = await ethers.getSigners(); //deployer es la cuenta que se usa para desplegar el contrato
-  console.log("Deploying contracts with the account:", deployer.address);
+  const [deployer] = await ethers.getSigners();
+  console.log(`Owner address: ${deployer.address}`);
 
-  //deploy DAppToken
-  const DAppToken = await ethers.getContractFactory("DAppToken");
-  const dAppToken = await DAppToken.deploy(deployer.address); // Pasar parámetros al constructor
+  console.log("Deploying LPToken...");
+  const lpTokenFactory = await ethers.getContractFactory("LPToken");
+  const lpToken = await lpTokenFactory.deploy(deployer.address);
+  await lpToken.waitForDeployment(); // Cambiado por ethers.js v6
+  console.log("LPToken deployed to:", lpToken.target); // target reemplaza address en ethers.js v6
 
-  await dAppToken.deployed(); // Esperar el despliegue
+  console.log("Deploying DAppToken...");
+  const dAppTokenFactory = await ethers.getContractFactory("DAppToken");
+  const dAppToken = await dAppTokenFactory.deploy(deployer.address);
+  await dAppToken.waitForDeployment(); // Cambiado por ethers.js v6
+  console.log("DAppToken deployed to:", dAppToken.target); // target reemplaza address en ethers.js v6
+};
 
-  console.log("DAppToken deployed to:", dAppToken.address);
-
-  // Desplegar el contrato LPToken
-  const LPToken = await ethers.getContractFactory("LPToken");
-  const lpToken = await LPToken.deploy(deployer.address); // Pasar parámetros al constructor
-
-  await lpToken.deployed(); // Esperar el despliegue
-
-  console.log("LPToken deployed to:", lpToken.address);
-}
-main().catch((error) => { 
+main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
